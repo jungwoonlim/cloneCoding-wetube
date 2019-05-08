deca4fe357e1ce49076bef339efb8e9da021542d
jungwoonlim <wypo23@morpheus3d.co.kr>
2019-05-08 16:29:05 +0900

    My First Express Server

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log(`hi`);
+const express = require(`express`);
+const app = express();
+
+const PORT = 8080;
+
+function handleListening() {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+app.listen(PORT, handleListening);
