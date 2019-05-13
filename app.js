7be3f81630f4133a949cfd3f5d76e387b84a6eec
jungwoonlim <wypo23@naver.com>
2019-05-13 20:55:51 +0900

    Express Core: Routing

---
+++ app.js
@@ -0,0 +1,27 @@
+// const express = require(`express`);
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import { userRouter } from "./router";
+
+const app = express();
+
+const handleHome = (req, res) => res.send(`Hello from home`);
+
+const handleProfile = (req, res) => res.send(`you are on my profile`);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(morgan(`dev`));
+app.use(helmet());
+
+app.get(`/`, handleHome);
+
+app.get(`/profile`, handleProfile);
+
+app.use(`/user`, userRouter);
+
+export default app;
