ae731242f87e731c72837dcbb47f6debb7a74553
jungwoonlim <wypo23@naver.com>
2019-05-16 21:16:07 +0900

    just commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,6 @@ export const login = (req, res) => res.render("Login", { pageTitle: "Login" });
 export const logout = (req, res) =>
   res.render("Logout", { pageTitle: "Logout" });
 
-export const users = (req, res) => res.render("Users", { pageTitle: "Users" });
 export const userDetail = (req, res) =>
   res.render("User Detail", { pageTitle: "User Detaik" });
 export const editProfile = (req, res) =>
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -2,9 +2,6 @@ export const home = (req, res) => res.render("Home", { pageTitle: "Home" });
 export const search = (req, res) =>
   res.render("Search", { pageTitle: "Search" });
 
-export const videos = (req, res) =>
-  res.render("Videos", { pageTitle: "Videos" });
-
 export const upload = (req, res) =>
   res.render("Upload", { pageTitle: "Upload" });
 export const videoDetail = (req, res) =>
