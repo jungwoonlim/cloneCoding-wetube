e4a715510338061eb48c366fdaa7bf83666e39a8
jungwoonlim <wypo23@morpheus3d.co.kr>
2019-05-16 16:36:43 +0900

    MVC Pattern part Three

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
-import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -11,9 +11,9 @@ import routes from "./routes";
 
 const app = express();
 
+app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieParser());
 app.use(morgan(`dev`));
 app.use(helmet());
 
