410c5adc57facbea9cc7770492311543e384da17
jungwoonlim <wypo23@morpheus3d.co.kr>
2019-05-16 16:42:35 +0900

    recap

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ const app = express();
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan(`dev`));
 app.use(helmet());
+app.use(morgan(`dev`));
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter);
