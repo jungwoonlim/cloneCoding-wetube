afacb1795580f60ccc5b15ee297690d30d6d308b
jungwoonlim <wypo23@morpheus3d.co.kr>
2019-05-17 16:11:24 +0900

    Home Controller part Two

--- db.js
+++ db.js
@@ -3,8 +3,9 @@ export const videos = [
     id: 324393,
     title: "Video awesome",
     description: "This is something I love",
-    view: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 121212,
       name: "jungwoon",
@@ -15,8 +16,9 @@ export const videos = [
     id: 1212121,
     title: "Video super",
     description: "This is something I love",
-    view: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 121212,
       name: "jungwoon",
@@ -27,8 +29,9 @@ export const videos = [
     id: 55555,
     title: "Video nice",
     description: "This is something I love",
-    view: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 121212,
       name: "jungwoon",
@@ -39,8 +42,9 @@ export const videos = [
     id: 11111,
     title: "Video perfect",
     description: "This is something I love",
-    view: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    views: 24,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 121212,
       name: "jungwoon",
