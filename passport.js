443a4f9a2880688a7b92ee7e861d403e5a4aba79
jungwoonlim <wypo23@morpheus3d.co.kr>
2019-05-23 10:49:44 +0900

    Fix Github Log In part One

--- passport.js
+++ passport.js
@@ -8,7 +8,7 @@ passport.use(User.createStrategy());
 passport.use(
   new GithubStrategy({
     clientID: process.env.GH_ID,
-    clientSecret: porcess.env.GH_SECRET,
+    clientSecret: process.env.GH_SECRET,
     callbackURL: "http://localhost:4000/auth/github/callback"
   }),
   githubLoginCallback
