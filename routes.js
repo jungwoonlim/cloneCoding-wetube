d09994d99411fd5444cba0c9d07edce9a5dc9602
jungwoonlim <wypo23@morpheus3d.co.kr>
2019-05-29 15:01:17 +0900

    API Adding a Comment part One

--- routes.js
+++ routes.js
@@ -37,6 +37,7 @@ const FB_CALLBACK = "/auth/facebook/callback";
 
 const API = "/api";
 const REGISTER_VIEW = "/:id/view";
+const ADD_COMMENT = "/:id/comment";
 
 const routes = {
   home: HOME,
@@ -88,7 +89,8 @@ const routes = {
   me: ME,
 
   api: API,
-  registerView: REGISTER_VIEW
+  registerView: REGISTER_VIEW,
+  addComment: ADD_COMMENT
 };
 
 export default routes;
